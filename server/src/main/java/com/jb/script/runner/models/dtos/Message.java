cd1aa7a674feedae265d813b7c29d7ce7fa46369
Edi Bicu <bicuedici@gmail.com>
2023-11-25 16:23:49 +0200

    Added base code websockets

---
+++ server/src/main/java/com/jb/script/runner/models/dtos/Message.java
@@ -0,0 +1,15 @@
+package com.jb.script.runner.models.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String content;
+}
