3d383cb1c287c9af3bb02d43f8a4865adfec222c
Edi Bicu <bicuedici@gmail.com>
2023-12-17 01:34:08 +0200

    api ready not tested

--- server/src/main/java/com/jb/scriptrunner/services/ScriptRunnerServiceImpl.java
+++ server/src/main/java/com/jb/scriptrunner/services/ScriptRunnerServiceImpl.java
@@ -78,4 +78,48 @@ public class ScriptRunnerServiceImpl extends ScriptRunnerService {
             throw new Exception(e.getMessage());
         }
     }
+
+    @Override
+    public void runMultipleTimes(UUID uuid, String script, String command, int count) throws Exception {
+        long expectedTimePerScript = -1;
+
+        File file = new File("src/main/resources/" + uuid + ".kts");
+        String path = file.getAbsolutePath();
+
+        try (OutputStream outputStream = new FileOutputStream(file)) {
+            outputStream.write(script.replaceAll("\\u00a0", " ").getBytes());
+        }
+
+        command += " " + path;
+
+        while (count > 0) {
+            long startTime = System.currentTimeMillis();
+            Process process = new ProcessBuilder(command.split("\\s")).start();
+
+            Thread outputThread = new Thread(() -> {
+                sendMessages(uuid, process.getInputStream(), TypeOfMessage.OUTPUT);
+            });
+
+            Thread errorOutputThread = new Thread(() -> {
+                sendMessages(uuid, process.getErrorStream(), TypeOfMessage.ERROR);
+            });
+
+            outputThread.start();
+            errorOutputThread.start();
+
+            int exitCode = process.waitFor();
+            outputThread.join();
+            errorOutputThread.join();
+            simpMessagingTemplate.convertAndSend("/topic/script-output/" + uuid, new Message(String.valueOf(exitCode), TypeOfMessage.EXIT_CODE));
+            expectedTimePerScript = System.currentTimeMillis() - startTime;
+            System.out.println(count + ": " + expectedTimePerScript);
+            count--;
+        }
+
+        if (file.delete()) {
+            System.out.println("File deleted!");
+        } else {
+            System.out.println("File not deleted!");
+        }
+    }
 }
